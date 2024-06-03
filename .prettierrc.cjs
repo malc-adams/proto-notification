ef98a697331e513f83c875c5011592f0c12b5b0c
Malc Adams <malc@malcadams.co.uk>
2024-06-03 18:16:23 +0100

    base files

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+/**
+ * Prettier formatter configuration.
+ *
+ * @copyright 2022-2023 Practically.io All rights reserved
+ */
+module.exports = {
+    trailingComma: 'all',
+    singleQuote: true,
+    bracketSpacing: false,
+    tabWidth: 4,
+};
